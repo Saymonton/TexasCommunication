af0d18d0a284dc998dda94590bd206e46bc855e1
Saymonton Loback <saymonton.f.l@gmail.com>
2025-09-05 20:42:03 -0300

    Software: ADDED converters.

---
+++ Software/TemplateStudioTest/Helpers/BoolInverterConverter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace TemplateStudioTest.Helpers;
+public class BoolInverterConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) => value is bool boolValue && !boolValue;
+    
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}

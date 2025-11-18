6c68f61e39f0a34ce68915e26f0b04987cf52c08
Saymonton Loback <saymonton.f.l@gmail.com>
2025-11-18 16:11:30 -0300

    sei la

---
+++ Software/TemplateStudioTest.Core/Models/EventArgs/LedsDataReceivedEventArgs.cs
@@ -0,0 +1,25 @@
+﻿namespace TemplateStudioTest.Core.Models.EventArgs;
+public class LedsDataReceivedEventArgs : System.EventArgs
+{
+    public byte[] ByteArrayReceived {
+        get; private set;
+    }
+    public byte ByteReceived
+    {
+        get; private set;
+    }
+    public bool UseByteArray
+    {
+        get; private set;
+    }
+    public LedsDataReceivedEventArgs(byte? _byte)
+    {
+        ByteReceived = _byte ?? throw new ArgumentNullException("_byte argument cannot be null.");
+        UseByteArray= false;
+    }
+    public LedsDataReceivedEventArgs(byte[] _bytes)
+    {
+        ByteArrayReceived = _bytes ?? throw new ArgumentNullException("_bytes argument cannot be null.");
+        UseByteArray= true;
+    }
+}
